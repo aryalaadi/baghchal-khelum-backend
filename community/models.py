3233812a42c120ecdea6c44c005815fe4cbe80e5
aryalaadi <aryalaadi123@gmail.com>
2025-12-07 11:48:07 +0545

    community service

---
+++ community/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from core.database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    title = Column(String(255))
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
